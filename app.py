341401ab2b065cf6d561e4b19ea149d40db33170
brayanbets <brayanruiz.runt@gmail.com>
2026-02-16 12:25:32 -0500

    Update app.py

--- app.py
+++ app.py
@@ -37,5 +37,8 @@ def inicio():
 
     return render_template_string(HTML, partidos=partidos)
 
+import os
+
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)

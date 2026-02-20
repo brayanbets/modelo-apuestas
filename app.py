4b87d6a529875f6d1c9f60b855cc07fec2b7aa5f
brayanbets <brayanruiz.runt@gmail.com>
2026-02-20 10:39:43 -0500

    Update app.py

--- app.py
+++ app.py
@@ -67,10 +67,12 @@ def obtener_partidos_futbol(market):
         ligas = {"Sin datos": ["No hay partidos hoy"]}
     return ligas
     def obtener_partidos():
-        headers = {
+    headers = {
         "x-apisports-key": os.environ.get("API_KEY")
     }
+
     url = f"https://v3.football.api-sports.io/teams/statistics?league={league_id}&season={season}&team={team_id}"
+    
     r = requests.get(url, headers=headers, timeout=10)
     data = r.json()
 
